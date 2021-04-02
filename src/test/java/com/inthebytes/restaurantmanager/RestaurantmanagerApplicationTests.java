c91fafdac8dc11257ada2fa03399fdac54852106
lexinelson <lexi.nelson@smoothstack.com>
2021-04-02 14:19:27 -0700

    Initialized starting project - Restaurant Manager Service

---
+++ src/test/java/com/inthebytes/restaurantmanager/RestaurantmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inthebytes.restaurantmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
