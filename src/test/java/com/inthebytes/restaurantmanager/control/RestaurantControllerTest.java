17c32503a693712a1ecf03726d82bad82f76c75e
lexinelson <lexi.nelson@smoothstack.com>
2021-04-13 22:29:14 -0700

    ITB-23: Controller Test Cases

--- src/test/java/com/inthebytes/restaurantmanager/control/RestaurantControllerTest.java
+++ src/test/java/com/inthebytes/restaurantmanager/control/RestaurantControllerTest.java
@@ -1,5 +1,89 @@
 package com.inthebytes.restaurantmanager.control;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityExistsException;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.inthebytes.restaurantmanager.entity.Location;
+import com.inthebytes.restaurantmanager.entity.Restaurant;
+import com.inthebytes.restaurantmanager.service.RestaurantService;
+
+@WebMvcTest(RestaurantController.class)
+@AutoConfigureMockMvc
 public class RestaurantControllerTest {
 
+		@Autowired
+		private MockMvc mock;
+		
+		@MockBean
+		private RestaurantService service;
+		
+		private Restaurant makeRestaurantModel() {
+			Restaurant test = new Restaurant();
+			test.setName("Lexi's Burgers");
+
+			Location location = new Location();
+			location.setStreet("Main St.");
+			location.setUnit("123");
+			location.setCity("Sacramento");
+			location.setState("California");
+			location.setZipCode(95838);
+			test.setLocation(location);
+
+			test.setCuisine("Fast Food");
+
+			return test;
+		}
+
+		@Test
+		public void createRestaurantTest() throws JsonProcessingException, Exception {
+			Restaurant restaurant = makeRestaurantModel();
+			Restaurant result = restaurant;
+			result.setRestaurantId(22L);
+			when(service.createRestaurant(restaurant)).thenReturn(result);
+
+			mock.perform(post("/creator/restaurant/starter")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(new ObjectMapper().writeValueAsString(restaurant)))
+			.andExpect(MockMvcResultMatchers.status().isCreated())
+			.andExpect(MockMvcResultMatchers.header().string("Restaurant-ID", Matchers.containsString("22")));
+		}
+
+		@Test
+		public void createInvalidRestaurantTest() throws JsonProcessingException, Exception {
+			Restaurant restaurant = makeRestaurantModel();
+			when(service.createRestaurant(restaurant)).thenThrow(new IllegalArgumentException());
+
+			mock.perform(post("/creator/restaurant/starter")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(new ObjectMapper().writeValueAsString(restaurant)))
+			.andExpect(MockMvcResultMatchers.status().isNotAcceptable());
+		}
+		
+		@Test
+		public void createExistingRestaurantTest() throws JsonProcessingException, Exception {
+			Restaurant restaurant = makeRestaurantModel();
+			when(service.createRestaurant(restaurant)).thenThrow(new EntityExistsException());
+
+			mock.perform(post("/creator/restaurant/starter")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(new ObjectMapper().writeValueAsString(restaurant)))
+			.andExpect(MockMvcResultMatchers.status().isConflict());
+		}
 }
--- src/test/java/com/inthebytes/restaurantmanager/service/RestaurantServiceTest.java
+++ src/test/java/com/inthebytes/restaurantmanager/service/RestaurantServiceTest.java
@@ -1,5 +1,60 @@
 package com.inthebytes.restaurantmanager.service;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import javax.persistence.EntityExistsException;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.inthebytes.restaurantmanager.entity.Location;
+import com.inthebytes.restaurantmanager.entity.Restaurant;
+
 public class RestaurantServiceTest {
 
+	@Autowired
+	private MockMvc mock;
+	
+	@MockBean
+	private RestaurantService service;
+	
+	private Restaurant makeRestaurantModel() {
+		Restaurant test = new Restaurant();
+		test.setName("Lexi's Burgers");
+
+		Location location = new Location();
+		location.setStreet("Main St.");
+		location.setUnit("123");
+		location.setCity("Sacramento");
+		location.setState("California");
+		location.setZipCode(95838);
+		test.setLocation(location);
+
+		test.setCuisine("Fast Food");
+
+		return test;
+	}
+
+	@Test
+	public void createRestaurantTest() throws JsonProcessingException, Exception {
+		Restaurant restaurant = makeRestaurantModel();
+	}
+
+	@Test
+	public void createInvalidRestaurantTest() throws JsonProcessingException, Exception {
+		Restaurant restaurant = makeRestaurantModel();
+	}
+	
+	@Test
+	public void createExistingRestaurantTest() throws JsonProcessingException, Exception {
+		Restaurant restaurant = makeRestaurantModel();
+	}
 }
