7eef129eaead018db531097635bd27f82180ac54
lexinelson <lexi.nelson@smoothstack.com>
2021-05-10 17:00:03 -0700

    ITB-121: Added restaurant manager account service layer

--- src/main/java/com/inthebytes/restaurantmanager/service/RestaurantManagerService.java
+++ src/main/java/com/inthebytes/restaurantmanager/service/RestaurantManagerService.java
@@ -2,7 +2,13 @@ package com.inthebytes.restaurantmanager.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.inthebytes.restaurantmanager.dto.RestaurantDTO;
+import com.inthebytes.restaurantmanager.dto.UserDto;
+import com.inthebytes.restaurantmanager.entity.Restaurant;
+import com.inthebytes.restaurantmanager.entity.Role;
+import com.inthebytes.restaurantmanager.entity.User;
 import com.inthebytes.restaurantmanager.mapper.RestaurantMapper;
+import com.inthebytes.restaurantmanager.mapper.UserMapper;
 import com.inthebytes.restaurantmanager.repository.RestaurantDao;
 import com.inthebytes.restaurantmanager.repository.RoleDao;
 import com.inthebytes.restaurantmanager.repository.UserDao;
@@ -20,5 +26,60 @@ public class RestaurantManagerService {
 	
 	@Autowired
 	private RestaurantMapper restaurantMapper;
+	
+	@Autowired
+	private UserMapper userMapper;
+	
+	private Role findManagerRole() {
+		Role role = roleRepo.findByName("restaurant");
+		if (role != null)
+			return role;
+		role = roleRepo.findByName("manager");
+		return role;
+	}
+	
+	public RestaurantDTO addManager(Long restaurantId, UserDto user) {
+		Restaurant restaurant = restaurantRepo.findByRestaurantId(restaurantId);
+		User userEntity = userRepo.findByUsername(user.getUsername());
+		
+		if (userEntity == null)
+			return null;
+		userEntity.setRole(findManagerRole());
+		userEntity.setActive(true);
+		userEntity = userRepo.save(userEntity);
+		
+		restaurant.getManager().add(userEntity);
+		return restaurantMapper.convert(restaurantRepo.save(restaurant));
+		
+	}
+	
+	public RestaurantDTO addManager(Long restaurantId, Long userId) {
+		User userEntity = userRepo.findByUserId(userId);
+		if (userEntity == null)
+			return null;
+		return addManager(restaurantId, userMapper.convert(userEntity));
+	}
+	
+	public RestaurantDTO removeManager(Long restaurantId, UserDto user) {
+		Restaurant restaurant = restaurantRepo.findByRestaurantId(restaurantId);
+		User userEntity = userRepo.findByUsername(user.getUsername());
+		if (userEntity == null)
+			return null;
+		
+		userEntity.setActive(false);
+		userEntity = userRepo.save(userEntity);
+		
+		if (restaurant.getManager().remove(userEntity)) {
+			return restaurantMapper.convert(restaurantRepo.save(restaurant));
+		}
+		return null;
+	}
+	
+	public RestaurantDTO removeManager(Long restaurantId, Long userId) {
+		User userEntity = userRepo.findByUserId(userId);
+		if (userEntity == null)
+			return null;
+		return removeManager(restaurantId, userMapper.convert(userEntity));
+	}
 
 }
