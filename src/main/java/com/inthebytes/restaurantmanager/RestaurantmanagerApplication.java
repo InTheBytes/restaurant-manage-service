434860603c6fb5af6b55c9afee9ed16a1f5f42c6
lexinelson <lexi.nelson@smoothstack.com>
2021-06-13 19:47:42 -0700

    Removed DEV cors bypass -oops

--- src/main/java/com/inthebytes/restaurantmanager/RestaurantmanagerApplication.java
+++ src/main/java/com/inthebytes/restaurantmanager/RestaurantmanagerApplication.java
@@ -19,7 +19,7 @@ public class RestaurantmanagerApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOriginPatterns("https://*.stacklunch.com", "https://stacklunch.com", "http://localhost:4200")
+						.allowedOriginPatterns("https://*.stacklunch.com", "https://stacklunch.com")
 						.allowCredentials(true)
 						.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
 			}
